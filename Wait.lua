f4e584025344328aa9d1320296dfcca4b3a092bd
dsfdsfsdkkkj <115834356+fsdfdfgdfghSD@users.noreply.github.com>
2023-03-18 17:08:40 +0200

    Update Wait.lua

--- Wait.lua
+++ Wait.lua
@@ -4,42 +4,41 @@ local socket = require("path-to-module")
 -- @param time (number) The amount of time to wait in seconds
 -- @param multiplier (number) Optional multiplier for the wait time. Default is 1.
 -- @return (boolean) True if the wait time was successful, false otherwise.
-local function Wait(time, multiplier)
-  multiplier = multiplier or 1
+local function Wait(Time, Multiplier)
+  Multiplier = Multiplier or 1
 
   -- Ensure that the input is valid (non-negative number)
-  if type(time) ~= "number" or time < 0 then
+  if type(Time) ~= "number" or Time < 0 then
     error("Wait function expects a non-negative number as its argument.", 2)
   end
 
-  local waitTime = time * multiplier
+  local WaitTime = Time * Multiplier
 
   -- If the specified wait time is less than 0.01 seconds, use a busy loop to wait
-  if waitTime < 0.01 then
-    local start = socket.gettime()
-    while socket.gettime() - start < waitTime do end
+  if WaitTime < 0.01 then
+    local Start = socket.gettime()
+    while socket.gettime() - Start < WaitTime do end
     return true
   end
 
   -- If coroutine is running, use coroutine.yield() to wait
   if coroutine.running() then
-    local start = socket.gettime()
+    local Start = socket.gettime()
     repeat
-      local success, errorMsg = coroutine.resume(coroutine.running(), waitTime)
-      if not success then
-        error("Error during coroutine wait: " .. errorMsg, 2)
+      local Success, Result = coroutine.resume(coroutine.running(), WaitTime)
+      if not Success then
+        error("Error during coroutine wait: "..Result, 2)
       end
-    until socket.gettime() - start >= waitTime
+    until socket.gettime() - Start >= WaitTime
   else
     -- If coroutine is not running, use os.time() to wait
-    local start = os.time()
+    local Start = os.time()
     repeat
-      local now = os.time()
-      if now < start then
+      local Now = os.time()
+      if Now < Start then
         error("System clock went backwards during wait.", 2)
       end
-    until now - start >= waitTime
+    until Now - Start >= WaitTime
   end
-
   return true
 end
