facf27918e7dc0cb6a0f3e86787f89d457b90871
dsfdsfsdkkkj <115834356+fsdfdfgdfghSD@users.noreply.github.com>
2023-03-18 14:18:38 +0200

    Update Wait.lua

--- Wait.lua
+++ Wait.lua
@@ -1,16 +1,22 @@
+-- Wait function that pauses execution for a specified amount of time
+-- @param Time (number) The amount of time to wait in seconds
 local function Wait(Time)
+  -- Ensure that the input is valid (non-negative number)
   assert(type(Time) == "number" and Time >= 0, "Invalid wait time")
 
+  -- If the specified wait time is less than 0.01 seconds, use a busy loop to wait
   if Time < 0.01 then
     local Start = os.clock()
     while os.clock() - Start < Time do end
     return
   end
 
+  -- If coroutine is running, use coroutine.yield() to wait
   if coroutine.running() then
     local Start = os.clock()
     repeat coroutine.yield() until os.clock() - Start >= Time
   else
+    -- If coroutine is not running, use os.time() to wait
     local Start = os.time()
     repeat
     until os.time() - Start >= Time
