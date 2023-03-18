cdfb176d44d7c898c8d354829097b9aeae651023
dsfdsfsdkkkj <115834356+fsdfdfgdfghSD@users.noreply.github.com>
2023-03-18 18:31:56 +0200

    Update Wait.lua

--- Wait.lua
+++ Wait.lua
@@ -1,44 +1,19 @@
-local socket = require("path-to-module")
+local function Wait(Time)
+    if type(Time) ~= "number" or Time < 0 then
+        error("Wait function expects a non-negative number as its argument")
+    end
 
--- Wait function that pauses execution for a specified amount of time
+    if Time < 0.01 then
+        local Start = os.clock()
+        while os.clock() - Start < Time do end
+        return
+    end
 
--- Time (number) The amount of time to wait in seconds
--- Multiplier (number) Optional multiplier for the wait time. Default is 1.
-local function Wait(Time, Multiplier)
-  Multiplier = Multiplier or 1
-
-  -- Ensure that the input is valid (non-negative number)
-  if type(Time) ~= "number" or Time < 0 then
-    error("Wait function expects a non-negative number as its argument.", 2)
-  end
-
-  local WaitTime = Time * Multiplier
-
-  -- If the specified wait time is less than 0.01 seconds, use a busy loop to wait
-  if WaitTime < 0.01 then
-    local Start = socket.gettime()
-    while socket.gettime() - Start < WaitTime do end
-    return true
-  end
-
-  -- If coroutine is running, use coroutine.yield() to wait
-  if coroutine.running() then
-    local Start = socket.gettime()
-    repeat
-      local Success, Result = coroutine.resume(coroutine.running(), WaitTime)
-      if not Success then
-        error("Error during coroutine wait: "..Result, 2)
-      end
-    until socket.gettime() - Start >= WaitTime
-  else
-    -- If coroutine is not running, use os.time() to wait
-    local Start = os.time()
-    repeat
-      local Now = os.time()
-      if Now < Start then
-        error("System clock went backwards during wait.", 2)
-      end
-    until Now - Start >= WaitTime
-  end
-  return true
+    if coroutine.running() then
+        local Start = os.clock()
+        repeat coroutine.yield() until os.clock() - Start >= Time
+    else
+        local Start = os.time()
+        repeat until os.time() - Start >= Time
+    end
 end
